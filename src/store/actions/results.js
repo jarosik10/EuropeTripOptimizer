57f86eedab4230661615d897750e9652b7defb0c
jarosik10 <bartosz.jaros96@gmail.com>
2020-10-31 19:18:50 +0100

    Delete old comment

--- src/store/actions/results.js
+++ src/store/actions/results.js
@@ -28,7 +28,6 @@ export const fetchDistanceMatrix = (locations, capitalsOrder) => {
     return dispatch => {
         dispatch(fetchDistanceMatrixStart());
         const API_KEY = '5b3ce3597851110001cf62487537592427e7443b9c7833eb30532a89'
-        // const API_KEY = 'erroooor'
         const url = `https://api.openrouteservice.org/v2/matrix/driving-car?api_key=${API_KEY}`;
         const data = {
             "locations": locations,
